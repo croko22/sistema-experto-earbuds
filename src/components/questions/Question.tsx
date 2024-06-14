05d27ce585be4491399f4c8231c9503938266dcc
croko22 <kvchambit@gmail.com>
2024-06-13 22:39:21 -0500

    style: Update font size in Question component

--- src/components/questions/Question.tsx
+++ src/components/questions/Question.tsx
@@ -7,11 +7,11 @@ const Question: React.FC<QuestionProps> = ({ procesarRespuesta, sintoma }) => {
   return (
     <div className="mb-4">
       {sintoma ? (
-        <h3 className="mb-4 text-2xl font-bold text-center text-gray-800">
+        <h3 className="mb-4 text-xl font-bold text-center text-gray-800">
           ¿El audífono tiene {sintoma} ?
         </h3>
       ) : (
-        <h3 className="mb-4 text-2xl font-bold text-center text-gray-800">
+        <h3 className="mb-4 text-xl font-bold text-center text-gray-800">
           No hay más preguntas :({" "}
         </h3>
       )}
